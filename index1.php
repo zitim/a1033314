3425fd549fa84d64a514f57bb18075cf0c085d83
zitim <s911297@gmail.com>
2016-06-16 12:04:54 +0800

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,43 @@
+<?php
+
+
+header("Content-Type:text/html; charset=utf-8");
+echo "<head>";
+echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
+echo "</head>";
+
+class PostOffice{
+
+    function mailFiler(){
+        $myfile = fopen("string.txt", "r") or die("Unable to open file!");
+		echo fread($myfile,filesize("string.txt"));
+		fclose($myfile);
+    }
+    function mailRegex(){
+     	$myfile2 = fopen("string.txt", "r") or die("Unable to open file!");
+   //   	while(fgetc($myfile2)){
+		 //   echo fgetc($myfile2);
+		 // }
+       	while(fgetc($myfile2)=='\w'){
+		     echo fgetc($myfile2);
+		  }
+		  fclose($myfile2);
+     	
+    }	
+    function spiltroad(){
+        $myfile = fopen("string.txt", "r") or die("Unable to open file!");
+		echo fread($myfile,filesize("string.txt"));
+		fclose($myfile);
+    }
+}
+
+$PostOffice = new PostOffice;
+$PostOffice->mailFiler();
+echo "<br/>";
+$PostOffice = new PostOffice;
+$PostOffice->mailRegex();
+echo "<br/>";
+ $PostOffice = new PostOffice;
+ $PostOffice->spiltroad();
+
+?>
